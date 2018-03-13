bbc3379438664db5f4e34cf3aaf02553c3d741f3
Colin Matcham <cmatcham@gmail.com>
2018-03-14 12:05:04 +1300

    Update submissions page to new layout

--- submission-summary-page.php
+++ submission-summary-page.php
@@ -3,15 +3,16 @@
 $page_colour = get_post_meta(get_the_ID(), 'page-colour-theme', true);
 $header_content = get_post_meta(get_the_ID(), '_rwnz_extra_content', true);
 ?>
+
+	
 	<section role="header" class="header">
-		<section class="mainStory" style="background-color: <?php echo $page_colour;?>; position: relative;">
-		<img src="<?php the_post_thumbnail_url('page-header'); ?>" style="width: 60%; margin-left: 5%; margin-top:2%; margin-bottom: -2%"/>
-		<div id="page-header" style="position: absolute; padding-left: 70%;  top:8%;width: 90%">
-			<?php include( locate_template( 'searchform.php', false, false ) );?> 
-		<h1 style="color:white;"><?php the_title()?></h1>
-        <?php echo $header_content; ?>
-        </div>	
-		
+		<section class="mainStory header-content-wrapper">
+			<div class="header-image-wrapper">
+    			<div class="header-image-inner"><img class="img" src="<?php the_post_thumbnail_url('page-header'); ?>" /></div>
+			</div>
+			<div class="page-title-container">
+				<h1><?php the_title()?></h1>
+			</div>
 		</section>
 	</section>
 
@@ -28,61 +29,58 @@ $header_content = get_post_meta(get_the_ID(), '_rwnz_extra_content', true);
 	
 	$query = new WP_Query( $next_args );
 	if ( $query->have_posts() ) {
-	        $query->the_post();
-	        ?>
-	        <div class="row blog_summary">
-	        	<div class="col-sm-8">
-	        		<article class="news_archive">
-	        					<h1><a href="<?php the_permalink()?>"><?php the_title()?></a></h1>
-	        					<h3><?php the_date()?></h3>
-	        					<?php the_excerpt();?>
-	        					<div style="border-top: 4px solid <?php echo $page_colour;?>; margin-top: 20px;"></div>
-								<?php $query->the_post(); ?>
-								<div class="left">
-	        					<img src="<?php the_post_thumbnail_url('thumbnail') ?>"/>
-	        					<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
-	        					<h3><?php the_date()?></h3>
-	        					<?php the_excerpt() ?>
-	        					</div>
-
-								<?php $query->the_post(); ?>
-								<div class="right">
-	        					<img src="<?php the_post_thumbnail_url('thumbnail') ?>"/>
-	        					<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
-	        					<h3><?php the_date()?></h3>
-	        					<?php the_excerpt() ?>
-	        					</div>
-
-								<?php $query->the_post(); ?>
-								<div class="left">
-	        					<img src="<?php the_post_thumbnail_url('thumbnail') ?>"/>
-	        					<h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
-	        					<h3><?php the_date()?></h3>
-	        					<?php the_excerpt() ?>
-	        					</div>
-
-	        		</article>
-	        	</div>
-	        	<div class="col-sm-4" style="padding: 40px;margin-top: 50px;">
-	        		<div style="border-top: 4px solid <?php echo $page_colour;?>; padding-bottom: 20px;"></div>
-	        		<h2>ARCHIVE:</h2>
-		<ul class="archive"><?php
-			$args = array(
- 			   'post_type'    => 'submission',
-			    'type'         => 'monthly',
-			    'echo'         => 0
-			);
-			echo wp_get_archives($args);
-			?>
-		</ul>
-	        	
-	        	</div>
-	        </div>
-	<?php 
+	    ?>
+		<div class="container-fluid">
+			<div class="row blog_summary">
+				<div class="col-sm-9">
+					<article class="news_archive">
+						<div class="page-header-wrapper">
+							<h1><?php the_title() ?></h1>
+							<!-- <h4><?php the_content() ?></h4> -->
+						</div>
+						<?php while($query->have_posts()): $query->the_post(); ?>
+							<div class="post-content-wrapper">
+								<a href="<?php the_permalink()?>"><span class="link"></span></a>
+								<div class="post-image-thumb">
+									<?php if (has_post_thumbnail('') ): ?>
+									<?php the_post_thumbnail('') ?>
+									<?php else: ?>
+									<img src="<?php echo get_theme_file_uri('/img/placeholderthumbnail.png')?>" alt="">
+									<?php endif; ?>
+								</div>
+								<div class="post-list-info">
+									<div>
+										<h2 class="post-list-title"><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
+										<h3><?php the_date()?></h3>
+										<p><?php the_excerpt() ?></p>
+									</div>
+								</div>
+							</div>
+						<?php endwhile;?>
+					</article>
+				</div>
+				<div class="col-sm-3 archive-wrapper" style="padding-right: 40px; margin-top: 50px;">
+					<h2>Archive</h2>
+					<ul class="archive">
+						<?php
+			             $args = array(
+ 			                'post_type'    => 'submission',
+			                 'type'         => 'monthly',
+			                 'echo'         => 0
+                			);
+                			echo wp_get_archives($args);
+			             ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<?php
 	    wp_reset_postdata();
 	} else {
 	    // none were found
 	}
+	
+	
 ?>
 
 	
